20c0c72536de01e20e50beeaac8e1fa15205ad8c
Ziyankhan2801 <ziyankhan1363@gmail.com>
2025-12-27 23:46:41 +0530

    Update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,28 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+# ================== CORS (CORE FIX) ==================
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+
+
+
+
 # ================== TEMPLATES ==================
 
 TEMPLATES = [

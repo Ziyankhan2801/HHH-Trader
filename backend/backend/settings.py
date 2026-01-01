14f4d49107c42ff6e4228de78d47eb29b76787af
Ziyankhan2801 <ziyankhan1363@gmail.com>
2026-01-01 14:23:22 +0530

    Safe DB config: sqlite local, postgres render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,8 @@
+import os
+import dj_database_url
+
+
+
 from pathlib import Path
 
 # ================== BASE ==================
@@ -85,12 +90,25 @@ TEMPLATES = [
 
 # ================== DATABASE ==================
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    # ✅ LOCAL DEV (SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # ================== STATIC ==================
 
